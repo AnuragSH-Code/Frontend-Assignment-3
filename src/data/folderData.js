a90e59cc3591a4013ea7a08970abba0c5172ce9a
AnuragSH-Code <anurag.sharma@brevo.com>
2025-07-03 16:42:21 +0530

    accordion

---
+++ src/data/folderData.js
@@ -0,0 +1,52 @@
+const explorer = {
+  id: "1",
+  name: "EM",
+  items: [
+    {
+      id: "2",
+      name: "GM",
+      items: [
+        {
+          id: "3",
+          name: "PM",
+          items: []
+        }
+      ]
+    },
+    {
+      id: "4",
+      name: "AM",
+      items: [
+        {
+          id: "5",
+          name: "IM",
+          items: []
+        }
+      ]
+    },
+    {
+      id: "6",
+      name: "QM",
+      items: [
+        {
+          id: "7",
+          name: "OM",
+          items: []
+        }
+      ]
+    },
+    {
+      id: "8",
+      name: "FM",
+      items: [
+        {
+          id: "9",
+          name: "JM",
+          items: []
+        }
+      ]
+    }
+  ]
+};
+
+export default explorer;
